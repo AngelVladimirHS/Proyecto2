764f3d4b1908ac8d8cead17362ca9b0731ffcd36
Angel Vladimir Hernandez Santiago <zs20019096@estudiantes.uv.mx>
2023-06-12 19:19:52 -0600

    Revert "Merge branch 'Develop'"
    This reverts commit 653b6b8a287155463f005982d274871ffada44a7, reversing
    changes made to dc11eed1b9cea07b75bb5ed98966512dc2bd23d6.

--- Practica1/test/practica1/OperacionesTest.java
+++ Practica1/test/practica1/OperacionesTest.java
@@ -21,7 +21,7 @@ public class OperacionesTest {
     @Test
     public void testObtenerNumeros() {
         System.out.println("obtenerNumeros");
-        List<Integer> expResult = new ArrayList<>(List.of(2,4,6,8));
+        List<Integer> expResult = new ArrayList<>(List.of(3,5,7,9));
         List<Integer> result = Operaciones.obtenerNumeros(1, 10);
         assertEquals(expResult, result);
     }
@@ -32,7 +32,7 @@ public class OperacionesTest {
     @Test
     public void testObtenerNumeros2() {
         System.out.println("obtenerNumeros");
-        List<Integer> expResult = new ArrayList<>(List.of(22,24,26,28));
+        List<Integer> expResult = new ArrayList<>(List.of(21,23,25,27,29));
         List<Integer> result = Operaciones.obtenerNumeros(20, 30);
         assertEquals(expResult, result);
     }
